02a5856bd13845202901bf02e0619df4f9d56e7c
sanskark <sanskarkhunt42@gmail.com>
2026-05-20 22:08:59 +0530

    Other services boilerplate

--- services/upload-service/main.py
+++ services/upload-service/main.py
@@ -1,7 +1,10 @@
+import json
+
 from fastapi import FastAPI, UploadFile, File
 import boto3
 import os
 from dotenv import load_dotenv
+from kafka import KafkaProducer
 
 load_dotenv(os.path.join(os.path.dirname(__file__), '..', '..', '.env'))
 
@@ -15,24 +18,38 @@ s3 = boto3.client(
     region_name=os.getenv("AWS_REGION"),
 )
 
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
 RAW_BUCKET = os.getenv("S3_RAW_BUCKET")
 
 
 @app.get("/health")
 def health():
-    return {"status": "OK"}
+    return {"status": "OK!"}
 
 
 @app.post("/upload")
 async def upload_video(file: UploadFile = File(...)):
     s3_key = f"raw/{file.filename}"
 
+    # Upload to S3
     s3.upload_fileobj(file.file, RAW_BUCKET, s3_key)
     print(f"Uploaded to S3: {s3_key}")
 
-    print(f"Received file: {file.filename}")
+    # Send Kafka event
+    producer.send("video.uploaded", {
+        "filename": file.filename,
+        "s3_key": s3_key,
+        "bucket": RAW_BUCKET
+    })
+    producer.flush()
+    print(f"Kafka event sent for: {file.filename}")
+
     return {
         "filename": file.filename,
         "s3_key": s3_key,
-        "message": "Uploaded to S3!"
+        "message": "Uploaded and queued for transcoding!"
     }
